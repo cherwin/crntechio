ff110d0138a268eef10ed85c57096368a749ad58
Cherwin Nooitmeer <cherwin@gmail.com>
2018-07-07 11:04:48 +0100

    Revert back to one host for testing purposes

--- terraform/README.md
+++ terraform/README.md
@@ -7,4 +7,8 @@ Prereqs
 
 Get image list
 ==============
-curl -X GET --silent "https://api.digitalocean.com/v2/images?per_page=999" -H "Authorization: Bearer $TF_VAR_do_token" |jq '.'
+curl -X GET --silent "https://api.digitalocean.com/v2/images?per_page=999" -H "Authorization: Bearer $TF_VAR_do_token" | jq
+
+Get public keys
+===============
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TF_VAR_do_token" "https://api.digitalocean.com/v2/account/keys" | jq
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,24 +4,27 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_tag" "consul-cluster-0" {
-  name = "consul-cluster-0"
+resource "digitalocean_tag" "consul" {
+  name = "consul"
 }
 
 resource "digitalocean_droplet" "node0" {
-  image  = "centos-7-x64"
-  name   = "node0"
-  region = "lon1"
-  size   = "512mb"
-  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
+  image    = "centos-7-x64"
+  name     = "node0"
+  region   = "lon1"
+  size     = "512mb"
+  tags     = ["${digitalocean_tag.consul.id}"]
+  ssh_keys = [20167863]
 }
 
+/*
 resource "digitalocean_droplet" "node1" {
   image  = "centos-7-x64"
   name   = "node1"
   region = "lon1"
   size   = "512mb"
-  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
+  tags   = ["${digitalocean_tag.consul.id}"]
+  ssh_keys = [ 20167863 ]
 }
 
 resource "digitalocean_droplet" "node2" {
@@ -29,5 +32,8 @@ resource "digitalocean_droplet" "node2" {
   name   = "node2"
   region = "lon1"
   size   = "512mb"
-  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
+  tags   = ["${digitalocean_tag.consul.id}"]
+  ssh_keys = [ 20167863 ]
 }
+*/
+
