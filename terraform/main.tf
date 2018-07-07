fcb5c0a235562d9b30ed7b30629d8e73e34261a6
Cherwin Nooitmeer <cherwin@gmail.com>
2018-07-07 10:40:44 +0100

    Provision consul VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,30 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+resource "digitalocean_tag" "consul-cluster-0" {
+  name = "consul-cluster-0"
+}
+
 resource "digitalocean_droplet" "node0" {
   image  = "centos-7-x64"
   name   = "node0"
   region = "lon1"
   size   = "512mb"
+  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
+}
+
+resource "digitalocean_droplet" "node1" {
+  image  = "centos-7-x64"
+  name   = "node1"
+  region = "lon1"
+  size   = "512mb"
+  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
+}
+
+resource "digitalocean_droplet" "node2" {
+  image  = "centos-7-x64"
+  name   = "node2"
+  region = "lon1"
+  size   = "512mb"
+  tags   = ["${digitalocean_tag.consul-cluster-0.id}"]
 }
